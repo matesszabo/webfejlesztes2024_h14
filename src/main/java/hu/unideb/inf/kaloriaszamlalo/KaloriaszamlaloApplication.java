5337bd1cd49e9567b946618d51665b8545ee8a3a
matesszabo <matesszabo@hotmail.com>
2024-10-07 15:26:31 +0200

    project init

---
+++ src/main/java/hu/unideb/inf/kaloriaszamlalo/KaloriaszamlaloApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.kaloriaszamlalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaloriaszamlaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaloriaszamlaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/kaloriaszamlalo/KaloriaszamlaloApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.kaloriaszamlalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaloriaszamlaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
