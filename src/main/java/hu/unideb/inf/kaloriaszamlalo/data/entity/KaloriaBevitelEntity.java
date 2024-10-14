91b02151207afd18b49a5dc9a6e9d104616743a2
matesszabo <matesszabo@hotmail.com>
2024-10-14 15:09:41 +0200

    szemelyEntity

--- src/main/java/hu/unideb/inf/kaloriaszamlalo/data/entity/KaloriaBevitelEntity.java
+++ src/main/java/hu/unideb/inf/kaloriaszamlalo/data/entity/KaloriaBevitelEntity.java
@@ -3,6 +3,7 @@ package hu.unideb.inf.kaloriaszamlalo.data.entity;
 import jakarta.persistence.*;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Table(name="kaloriabevitel")
@@ -17,6 +18,61 @@ public class KaloriaBevitelEntity {
     private Date datum;
 
     @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "etel_id", referencedColumnName = "id")
     private EtelEntity etel;
 
+    public KaloriaBevitelEntity() {
+    }
+
+    public KaloriaBevitelEntity(long id, Float suly, Date datum, EtelEntity etel) {
+        this.id = id;
+        this.suly = suly;
+        this.datum = datum;
+        this.etel = etel;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Float getSuly() {
+        return suly;
+    }
+
+    public void setSuly(Float suly) {
+        this.suly = suly;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+
+    public EtelEntity getEtel() {
+        return etel;
+    }
+
+    public void setEtel(EtelEntity etel) {
+        this.etel = etel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        KaloriaBevitelEntity that = (KaloriaBevitelEntity) o;
+        return id == that.id && Objects.equals(suly, that.suly) && Objects.equals(datum, that.datum) && Objects.equals(etel, that.etel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, suly, datum, etel);
+    }
 }
---
+++ src/main/java/hu/unideb/inf/kaloriaszamlalo/data/entity/SzemelyEntity.java
@@ -0,0 +1,109 @@
+package hu.unideb.inf.kaloriaszamlalo.data.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "szemely_adat")
+public class SzemelyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "nev")
+    private String nev;
+    @Column(name = "szuletesi_datum")
+    private Date szuletesi;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "jelszo")
+    private String jelszo;
+    @Column(name = "magassag")
+    private Integer magassag;
+    @Column(name = "nem")
+    private String nem;
+
+    public SzemelyEntity() {
+    }
+
+    public SzemelyEntity(long id, String nev, Date szuletesi, String email, String jelszo, Integer magassag, String nem) {
+        this.id = id;
+        this.nev = nev;
+        this.szuletesi = szuletesi;
+        this.email = email;
+        this.jelszo = jelszo;
+        this.magassag = magassag;
+        this.nem = nem;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public Date getSzuletesi() {
+        return szuletesi;
+    }
+
+    public void setSzuletesi(Date szuletesi) {
+        this.szuletesi = szuletesi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJelszo() {
+        return jelszo;
+    }
+
+    public void setJelszo(String jelszo) {
+        this.jelszo = jelszo;
+    }
+
+    public Integer getMagassag() {
+        return magassag;
+    }
+
+    public void setMagassag(Integer magassag) {
+        this.magassag = magassag;
+    }
+
+    public String getNem() {
+        return nem;
+    }
+
+    public void setNem(String nem) {
+        this.nem = nem;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SzemelyEntity that = (SzemelyEntity) o;
+        return id == that.id && Objects.equals(nev, that.nev) && Objects.equals(szuletesi, that.szuletesi) && Objects.equals(email, that.email) && Objects.equals(jelszo, that.jelszo) && Objects.equals(magassag, that.magassag) && Objects.equals(nem, that.nem);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nev, szuletesi, email, jelszo, magassag, nem);
+    }
+}
