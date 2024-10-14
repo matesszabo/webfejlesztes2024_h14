54b846d3e06f0f15d4986b7f7a94d41a4e9108cd
matesszabo <matesszabo@hotmail.com>
2024-10-14 14:24:32 +0200

    etelcontroller, spring security disable

--- src/main/java/hu/unideb/inf/kaloriaszamlalo/KaloriaszamlaloApplication.java
+++ src/main/java/hu/unideb/inf/kaloriaszamlalo/KaloriaszamlaloApplication.java
@@ -2,8 +2,9 @@ package hu.unideb.inf.kaloriaszamlalo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class KaloriaszamlaloApplication {
 
 	public static void main(String[] args) {
