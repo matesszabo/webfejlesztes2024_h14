259f77b6df3fdcf9348d256d97a9dc58ed8bcce4
matesszabo <matesszabo@hotmail.com>
2024-10-28 15:08:34 +0100

    Etel crud controller

--- src/main/java/hu/unideb/inf/kaloriaszamlalo/controller/EtelController.java
+++ src/main/java/hu/unideb/inf/kaloriaszamlalo/controller/EtelController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController //json-t küld, json-t fogad
 @RequestMapping("/api")
 public class EtelController {
@@ -35,6 +38,37 @@ public class EtelController {
             return repository.save(entity);
         }
         return null;
+    }
+
+    // /localhost:8080/api/etel?id=1
+    @DeleteMapping("/etel")
+    public void deleteById(@RequestParam long id){
+        repository.deleteById(id);
+        //repository.delete(Etel);
+    }
+
+    @GetMapping("/etel")
+    public List<EtelEntity> findAll(){
+        return repository.findAll();
+    }
+
+
+    // localhost:8080/api/etel/reggeli
+    @GetMapping("/etel/{tipus}")
+    public List<EtelEntity> findByTipus(@PathVariable String tipus){
+        List<EtelEntity> szurt = new ArrayList<>();
+        szurt = repository.findAll()
+                .stream()
+                .filter(x -> x.getTipus().equals(tipus))
+                .toList();
+
+        return szurt;
+    }
+
+    // localhost:8080/api/eteltipus?tipus=reggeli
+    @GetMapping("/eteltipus")
+    public List<EtelEntity> findByTipusRp(@RequestParam String tipus){
+        return repository.findAllByTipus(tipus);
     }
 
 
--- src/main/java/hu/unideb/inf/kaloriaszamlalo/data/repository/EtelRepository.java
+++ src/main/java/hu/unideb/inf/kaloriaszamlalo/data/repository/EtelRepository.java
@@ -4,6 +4,10 @@ import hu.unideb.inf.kaloriaszamlalo.data.entity.EtelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EtelRepository extends JpaRepository<EtelEntity, Long> {
+    //select * from Etel where tipus = ?tipus
+    List<EtelEntity> findAllByTipus(String tipus);
 }
