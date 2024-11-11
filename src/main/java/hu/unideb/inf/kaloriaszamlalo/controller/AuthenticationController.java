3f0ee9e677e04a0281fcca342f2dbfebb8798b43
matesszabo <matesszabo@hotmail.com>
2024-11-11 15:05:30 +0100

    spring security config progress 4 regisztracio

---
+++ src/main/java/hu/unideb/inf/kaloriaszamlalo/controller/AuthenticationController.java
@@ -0,0 +1,22 @@
+package hu.unideb.inf.kaloriaszamlalo.controller;
+
+import hu.unideb.inf.kaloriaszamlalo.service.AuthenticationService;
+import hu.unideb.inf.kaloriaszamlalo.service.dto.RegisztracioDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/regisztracio")
+    public void regisztracio(@RequestBody RegisztracioDto dto){
+        authenticationService.regisztracio(dto);
+    }
+}
